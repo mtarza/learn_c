d22b82ac80b5dcb7a9f6989c84a15fc58712b030
tarzacol <amintazrae@gmail.com>
2024-10-02 10:57:08 +0100

    update

--- Preprocessor/e6.c
+++ Preprocessor/e6.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#ifndef DEBUG
+// #ifndef DEBUG
 
-//#warning DUBY
-//#error dibag
-#endif
-int main()
-{
-    printf("hello world!!");
+// //#warning DUBY
+// //#error dibag
+// #endif
+int main(void) {
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
